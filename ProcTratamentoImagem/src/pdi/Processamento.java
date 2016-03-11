1e98cee7ae73605085cc70b1d30a365a967b4d10
roberto.bento <rbdinamite@gmail.com>
2016-03-10 21:48:56 -0300

    Adicionada função de filtro por Mediana
    * Problema na geração da image (toda preta)

---
+++ ProcTratamentoImagem/src/pdi/Processamento.java
@@ -0,0 +1,114 @@
+package pdi;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+
+import utils.Pixel;
+
+public class Processamento {
+
+	public BufferedImage filtroMediana(int tipoVizinho, BufferedImage image) {
+		System.out.println("Chegou na função -> filtroMediana");
+		WritableRaster raster = image.getRaster();
+		BufferedImage newImg = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+		int pixels[] = new int[4];
+		System.out.println("Iniciando loop pelo tamanho da imagem");
+		for (int i = 1; i < image.getWidth()-1; i++) {
+			for (int j = 1; j <image.getHeight()-1; j++) {
+				raster.getPixel(i,j,pixels);
+				int[] medianas = verificaTipoMediana(tipoVizinho, image, i, j);
+				pixels[0] = medianas[0];
+				pixels[1] = medianas[1];
+				pixels[2] = medianas[2];
+				raster.setPixel(i,j,pixels);
+			}
+		}
+		try {
+			newImg.setData(raster);
+		} catch (Exception e) {
+			System.out.println("Erro ao filtrar imagem");
+			e.printStackTrace();
+		}
+		return newImg;
+	}
+	
+	public int[] verificaTipoMediana(int tipo, BufferedImage image, int x, int y) {
+		switch (tipo) {
+		case 1:
+			return calculaMediana3X3(image, x, y);
+		default:
+			return null;
+		}
+	}
+	
+	public int[] calculaMediana3X3(BufferedImage image, int x, int y) {
+		Pixel[][] vizinhos = criaMatrizVizinho3X3(x, y);
+		WritableRaster raster = image.getRaster();
+		int[] pixels = new int[4];
+		for (int i = 0; i < vizinhos.length; i++) {
+			for (int j = 0; i < vizinhos.length; i++) {
+				raster.getPixel(vizinhos[i][j].getX(), vizinhos[i][j].getY(), pixels);
+				vizinhos[i][j].setR(pixels[0]);
+				vizinhos[i][j].setG(pixels[1]);
+				vizinhos[i][j].setB(pixels[2]);
+			}
+		}
+		pixels[0] = retornaMediana(vizinhos, 0);
+		pixels[1] = retornaMediana(vizinhos, 1);
+		pixels[2] = retornaMediana(vizinhos, 2);
+		return pixels;
+	}
+	
+	public int retornaMediana(Pixel[][] vizinhos, int indice) {
+		int[] valores = new int[9];
+		int cont = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				switch (indice) {
+				case 0:
+					valores[cont] = vizinhos[i][j].getR();
+					break;
+				case 1:
+					valores[cont] = vizinhos[i][j].getG();
+					break;
+				case 2:
+					valores[cont] = vizinhos[i][j].getB();
+					break;
+				default:
+					break;
+				}
+				cont++;
+			}
+		}
+		valores = ordenaVetor(valores);
+		return valores[4];
+	}
+	
+	public int[] ordenaVetor(int[] vetor) {
+		int aux = 0;
+		for (int i = 0; i < vetor.length; i++) { 
+			for (int j = 0; j < vetor.length; j++) { 
+				if (vetor[i] < vetor[j]) { 
+					aux = vetor[i]; 
+					vetor[i] = vetor[j]; 
+					vetor[j] = aux; 
+				} 
+			}
+		}
+		return vetor;
+	}
+	
+	public Pixel[][] criaMatrizVizinho3X3(int x, int y) {
+		Pixel[][] vizinhos = new Pixel[3][3];
+		vizinhos[0][0] = new Pixel(x-1, y-1);
+		vizinhos[0][1] = new Pixel(x-1, y);
+		vizinhos[0][2] = new Pixel(x-1, y+1);
+		vizinhos[1][0] = new Pixel(x, y-1);
+		vizinhos[1][1] = new Pixel(x, y);
+		vizinhos[1][2] = new Pixel(x, y+1);
+		vizinhos[2][0] = new Pixel(x+1, y-1);
+		vizinhos[2][1] = new Pixel(x+1, y);
+		vizinhos[2][2] = new Pixel(x+1, y+1);
+		return vizinhos;
+	}
+}
--- ProcTratamentoImagem/src/view/Principal.java
+++ ProcTratamentoImagem/src/view/Principal.java
@@ -1,6 +1,11 @@
-package tela1;
+package view;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
@@ -8,12 +13,17 @@ import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wb.swt.SWTResourceManager;
+
+import pdi.Processamento;
+import utils.Funcoes;
+
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.MouseMoveListener;
 import org.eclipse.swt.events.MouseEvent;
@@ -21,7 +31,6 @@ import org.eclipse.swt.events.MouseAdapter;
 
 public class Principal extends Shell {
 	private CLabel lblImagem1;
-	private CLabel lblImagem2;
 	private Button btnImagem1;
 	private Button btnImagem2;
 	private Label lblNewLabel;
@@ -29,12 +38,15 @@ public class Principal extends Shell {
 	private Label lblB;
 	private Label lblG;
 	private ScrolledComposite scrolledComposite;
-	private ScrolledComposite scrolledComposite_1;
 	private ScrolledComposite scrolledComposite_2;
 	private CLabel lblImagem3;
 	private Label lblCor;
 	private Label lblPos;
 	ArrayList<String> listaCordenadas = new ArrayList<String>(); 
+	static Funcoes funcoes;
+	private ScrolledComposite scrolledComposite_1;
+	private CLabel lblImagem2;
+	private Button btnMediana2;
 	
 
 	/**
@@ -45,7 +57,7 @@ public class Principal extends Shell {
 		try {
 			Display display = Display.getDefault();
 			Principal shell = new Principal(display);
-			new Funcoes(shell);
+			funcoes = new Funcoes(shell);
 			shell.open();
 			shell.layout();
 			while (!shell.isDisposed()) {
@@ -72,8 +84,42 @@ public class Principal extends Shell {
 		tabFolder.setBounds(10, 10, 932, 144);
 		tabFolder.setSelectionBackground(Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT));
 		
-		CTabItem tabItem = new CTabItem(tabFolder, SWT.NONE);
-		tabItem.setText("New Item");
+		CTabItem acaoFiltro = new CTabItem(tabFolder, SWT.NONE);
+		acaoFiltro.setText("Filtro");
+		
+		Composite compositeFiltro = new Composite(tabFolder, SWT.NONE);
+		acaoFiltro.setControl(compositeFiltro);
+		
+		Button btnMediana1 = new Button(compositeFiltro, SWT.NONE);
+		btnMediana1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				Processamento proc = new Processamento();
+				try {
+					System.out.println("Arquivo -> "+funcoes.getFilePath1());
+					BufferedImage mediana = proc.filtroMediana(1, ImageIO.read(new File(funcoes.getFilePath1())));
+					ImageIO.write(mediana, "jpg", new File("images/_mediana.jpg"));
+					Image Image3 = new Image(null, "images/_mediana.jpg");
+					funcoes.setImage3(Image3);
+					funcoes.abreImagem(3, lblImagem3);
+				} catch (IOException e) {
+					System.out.println("Erro iniciar função de filtro");
+					e.printStackTrace();
+				}
+			}
+		});
+		btnMediana1.setBounds(10, 22, 168, 32);
+		btnMediana1.setText("Fun\u00E7\u00E3o mediana - Imagem 1");
+		
+		btnMediana2 = new Button(compositeFiltro, SWT.NONE);
+		btnMediana2.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				//APLICA FUNÇÃO MEDIANA NA IMAGEM 2
+			}
+		});
+		btnMediana2.setText("Fun\u00E7\u00E3o mediana - Imagem 2");
+		btnMediana2.setBounds(10, 60, 168, 32);
 		
 		CTabItem tabItem_2 = new CTabItem(tabFolder, SWT.NONE);
 		tabItem_2.setText("New Item");
@@ -90,7 +136,7 @@ public class Principal extends Shell {
 		btnImagem1.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				Funcoes.abreImagem(1,lblImagem1);
+				funcoes.abreImagem(1,lblImagem1);
 			}
 		});
 		btnImagem1.setBounds(10, 160, 75, 25);
@@ -100,6 +146,7 @@ public class Principal extends Shell {
 		btnImagem2.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
+				funcoes.abreImagem(2,lblImagem2);
 			}
 		});
 		btnImagem2.setText("Imagem 2");
@@ -124,43 +171,48 @@ public class Principal extends Shell {
 		lblB.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.BOLD));
 		lblB.setBounds(517, 160, 62, 25);
 		
-		scrolledComposite = new ScrolledComposite(composite, SWT.H_SCROLL | SWT.V_SCROLL);
+		scrolledComposite = new ScrolledComposite(composite, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite.setBounds(10, 191, 287, 241);
-		scrolledComposite.setExpandHorizontal(true);
-		scrolledComposite.setExpandVertical(true);
-		lblImagem1 = new CLabel(scrolledComposite, SWT.BORDER);
+		lblImagem1 = new CLabel(scrolledComposite, SWT.NONE);
+		scrolledComposite.setContent(lblImagem1);
+		scrolledComposite.setMinSize(lblImagem1.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 		lblImagem1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseUp(MouseEvent arg0) {
-				listaCordenadas.add(lblPos.getText());
-				System.out.println("add -> "+lblPos.getText());
+				String texto = lblPos.getText()+","+lblR.getText().substring(3)+","+lblG.getText().substring(3)+","+lblB.getText().substring(3);
+				listaCordenadas.add(texto);
+				System.out.println("add -> "+texto);
 			}
 		});
-		scrolledComposite.setContent(lblImagem1);
-		//scrolledComposite.setMinSize(lblImagem1.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		scrolledComposite.setMinSize(lblImagem1.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 		lblImagem1.addMouseMoveListener(new MouseMoveListener() {
 			public void mouseMove(MouseEvent arg0) {
-				int x = scrolledComposite.toControl(display.getCursorLocation()).x;
-				int y = scrolledComposite.toControl(display.getCursorLocation()).y;
-				Funcoes.capturaPixel(x, y, lblPos, lblCor, lblR, lblG, lblB);
+				funcoes.capturaPixel(1,arg0.x, arg0.y, lblPos, lblCor, lblR, lblG, lblB);
 			}
 		});
 		
-		scrolledComposite_1 = new ScrolledComposite(composite, SWT.H_SCROLL | SWT.V_SCROLL);
-		scrolledComposite_1.setExpandVertical(true);
-		scrolledComposite_1.setExpandHorizontal(true);
+		scrolledComposite_1 = new ScrolledComposite(composite, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite_1.setBounds(303, 191, 287, 241);
 		
-		
-		
-		lblImagem2 = new CLabel(scrolledComposite_1, SWT.BORDER);
-		lblImagem2.setText("");
+		lblImagem2 = new CLabel(scrolledComposite_1, SWT.NONE);
 		scrolledComposite_1.setContent(lblImagem2);
 		scrolledComposite_1.setMinSize(lblImagem2.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		lblImagem2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent arg0) {
+				String texto = lblPos.getText()+","+lblR.getText().substring(3)+","+lblG.getText().substring(3)+","+lblB.getText().substring(3);
+				listaCordenadas.add(texto);
+				System.out.println("add -> "+texto);
+			}
+		});
+		scrolledComposite.setMinSize(lblImagem1.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		lblImagem2.addMouseMoveListener(new MouseMoveListener() {
+			public void mouseMove(MouseEvent arg0) {
+				funcoes.capturaPixel(2,arg0.x, arg0.y, lblPos, lblCor, lblR, lblG, lblB);
+			}
+		});
 		
-		scrolledComposite_2 = new ScrolledComposite(composite, SWT.H_SCROLL | SWT.V_SCROLL);
-		scrolledComposite_2.setExpandVertical(true);
-		scrolledComposite_2.setExpandHorizontal(true);
+		scrolledComposite_2 = new ScrolledComposite(composite, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite_2.setBounds(655, 191, 287, 241);
 		
 		lblImagem3 = new CLabel(scrolledComposite_2, SWT.BORDER);
@@ -174,6 +226,11 @@ public class Principal extends Shell {
 		lblPos = new Label(composite, SWT.NONE);
 		lblPos.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.BOLD));
 		lblPos.setBounds(655, 165, 75, 20);
+		
+		Label lblResultado = new Label(composite, SWT.NONE);
+		lblResultado.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.BOLD));
+		lblResultado.setBounds(828, 160, 89, 25);
+		lblResultado.setText("Resultado");
 		createContents();
 	}
 
